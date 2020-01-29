5e855fcdec4763626b8c769fb24d0f1ea8b67995
Alejandro <alejandro.munoz@saldum.com>
2020-01-29 15:58:09 +0100

    asdfds
    a

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -7,4 +7,5 @@ echo "######################"
 echo "*** Building image ***"
 echo "######################"
 
+export TAG=10
 cd docker/ && sudo docker-compose -f docker-compose-build.yml build --no-cache
