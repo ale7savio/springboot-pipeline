d4a2b7ec3348bfcbc9aac721840fb9c3dc524eea
Alejandro <alejandro.munoz@saldum.com>
2020-01-30 09:13:23 +0100

    asdf

--- jenkins/scripts/mvn.sh
+++ jenkins/scripts/mvn.sh
@@ -7,5 +7,4 @@ echo "****************"
 # -v Ruta pom desde donde se ejecuta:ruta donde se copia -w /ruta donde se ha copiado
 #docker ps -q --filter "name=name_docker" | grep -q . && docker stop name_docker && docker rm -fv name_docker
 docker rm maven-alpine
-
 docker run --name maven-alpine -v $(pwd):/app -w /app maven:3-alpine "$@"
