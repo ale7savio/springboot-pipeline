f7f233e34d8a87c00a7f62bbee6111a052c26403
Alejandro <alejandro.munoz@saldum.com>
2020-01-30 01:20:50 +0100

    xdsf

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -19,6 +19,9 @@
 
 #Deberia ser en el kubernetes donde se despliegua el proyecto
 
+#PRIMERO PARA QUE GENERE LAS KEYS
+#ssh alejandro_munoz@35.234.98.81
+
 #Copio el script que levanta el servicio
 #sudo chmod -R 777 jenkins/scripts/key/jenkinsinstancenopass
 scp -i ../../keys/jenkinsinstancenopass jenkins/scripts/publish.sh alejandro_munoz@35.234.98.81:/tmp/publish.sh
@@ -26,4 +29,4 @@ scp -i ../../keys/jenkinsinstancenopass jenkins/scripts/publish.sh alejandro_mun
 scp -i ../../keys/jenkinsinstancenopass jenkins/scripts/compose/docker-compose.yml alejandro_munoz@35.234.98.81:~/../apps/springboot-pipeline/
 scp -i ../../keys/jenkinsinstancenopass jenkins/scripts/compose/.env alejandro_munoz@35.234.98.81:~/../apps/springboot-pipeline/
 #Ejecuto el script que levanta el servicio
-#ssh -i ../../keys/jenkinsinstancenopass alejandro_munoz@35.234.98.81 /tmp/publish.sh
+ssh -i ../../keys/jenkinsinstancenopass alejandro_munoz@35.234.98.81 /tmp/publish.sh
