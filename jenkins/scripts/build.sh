5984ddaa98ff6e7ad426bbf3d8ac2a58d2cd6d65
Alejandro <alejandro.munoz@saldum.com>
2020-01-30 10:30:54 +0100

    asdfasdf

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -7,4 +7,7 @@ echo "######################"
 echo "*** Building image ***"
 echo "######################"
 
+#docker images --filter reference='*pringboot-pipeline*'
+docker rmi -f $(docker images --filter reference='*pringboot-pipeline*' -q)
+
 cd docker/ && docker-compose -f docker-compose-build.yml build --no-cache
