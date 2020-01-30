e93ba1b095ae407cea5f51882fe1d906701e52d8
Alejandro <alejandro.munoz@saldum.com>
2020-01-30 01:22:52 +0100

    sdsf

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -29,4 +29,6 @@ scp -i ../../keys/jenkinsinstancenopass jenkins/scripts/publish.sh alejandro_mun
 scp -i ../../keys/jenkinsinstancenopass jenkins/scripts/compose/docker-compose.yml alejandro_munoz@35.234.98.81:~/../apps/springboot-pipeline/
 scp -i ../../keys/jenkinsinstancenopass jenkins/scripts/compose/.env alejandro_munoz@35.234.98.81:~/../apps/springboot-pipeline/
 #Ejecuto el script que levanta el servicio
-ssh -i ../../keys/jenkinsinstancenopass alejandro_munoz@35.234.98.81 /tmp/publish.sh
+
+echo $BUILD_ID
+#ssh -i ../../keys/jenkinsinstancenopass alejandro_munoz@35.234.98.81 /tmp/publish.sh
